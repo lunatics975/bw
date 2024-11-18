8bfd04e6739871c0522c26090999f22b862e6ed5
Mingqi Li <mingqi_li@icloud.com>
2024-11-18 21:57:45 +0100

    final

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ def get_perioden(filename):
 
     file = open(filename, 'r')
     Lines = file.readlines()
-    Lines = Lines[1:]
+    print('Tabelle wird erstellt')
+    
     for i in range(len(Lines)):   
         Lines[i] = int(Lines[i])
-    return Lines
+    Num = Lines[0]
+    Lines = Lines[1:]
+    Lines.insert(0,0)
+    return Num, Lines
 
 def berechne_weg(perioden):
-    """
-    Berechnet den schnellsten Weg zum Grabmal.
-
-    Args:
-        perioden: Eine Liste von Ganzzahlen, die die Perioden der Steinquader in den Abschnitten des Ganges darstellen.
-
-    Returns:
-        Eine Liste von Tupeln, die die Instruktionen für Petra darstellen. Jedes Tupel enthält die Zeit in Minuten und die Aktion (Warten oder Weitergehen).
-    """
 
     anzahl_abschnitte = len(perioden)
     max_zeit = 2 * max(perioden) * anzahl_abschnitte  # Maximale Simulationszeit (kann angepasst werden)
@@ -25,9 +20,12 @@ def berechne_weg(perioden):
     quader_zustand = [[True] * anzahl_abschnitte for _ in range(max_zeit + 1)]
 
     # Simulation der Quaderbewegungen
-    for t in range(1, max_zeit + 1):
-        for i, periode in enumerate(perioden):
-            if (t // periode) % 2 == 0:  # Quader blockiert
+    for t in range(0, max_zeit + 1):        
+        for i in range (0,len(perioden)):
+            periode = perioden[i]
+            if periode == 0: # Quader frei
+               quader_zustand[t][i] = False
+            elif (t // periode) % 2 == 0:  # Quader blockiert
                 quader_zustand[t][i] = True
             else:  # Quader frei
                 quader_zustand[t][i] = False
@@ -37,23 +35,43 @@ def berechne_weg(perioden):
     fertige_wege = []  # Liste von abgeschlossenen Wegen zum Grabmal
 
     while wege:
+        
         aktueller_weg = wege.pop(0)
         aktuelle_zeit, aktuelle_position = aktueller_weg[-1]
 
-        if aktuelle_position == anzahl_abschnitte - 1:  # Grabmal erreicht
+        if aktuelle_position == anzahl_abschnitte-1:  # Grabmal erreicht
             fertige_wege.append(aktueller_weg)
-            continue
+            break
 
-        # Mögliche Aktionen: Warten oder Weitergehen
+        # Mögliche Aktionenarten oder Zurückgehen
         neuer_weg = aktueller_weg[:]        
         # Weitergehen
-        if (aktuelle_position + 1 < anzahl_abschnitte) and (aktuelle_zeit + 1 <= max_zeit) and \
-                (not quader_zustand[aktuelle_zeit + 1][aktuelle_position]) and \
-                (not quader_zustand[aktuelle_zeit + 1][aktuelle_position + 1]):            
-            neuer_weg.append((aktuelle_zeit + 1, aktuelle_position + 1))
-        else: #Warten
-            neuer_weg.append((aktuelle_zeit + 1, aktuelle_position))
-       
+    #    if (aktuelle_position + 1 < anzahl_abschnitte) and (aktuelle_zeit + 1 <= max_zeit):
+        if (not quader_zustand[aktuelle_zeit + 1][aktuelle_position + 1]):   #go
+            if (aktuelle_position + 1 == anzahl_abschnitte-1):
+                neuer_weg.append((aktuelle_zeit + 1, aktuelle_position + 1)) 
+            else:
+                for i in range (2, max(2,last_quader-aktuelle_position)+1):
+                    if (not quader_zustand[aktuelle_zeit + 1][aktuelle_position+i]) and (aktuelle_position + i == anzahl_abschnitte-1):
+                        neuer_weg.append((aktuelle_zeit + 1, aktuelle_position + i)) 
+                        break
+                    elif  (quader_zustand[aktuelle_zeit + 1][aktuelle_position+i] == True):
+                        neuer_weg.append((aktuelle_zeit + 1, aktuelle_position + i-1))        
+                        break
+                print(aktuelle_zeit, aktuelle_position)
+        elif  (not quader_zustand[aktuelle_zeit + 1][aktuelle_position]) or aktuelle_position ==0: #Warten
+                  neuer_weg.append((aktuelle_zeit + 1, aktuelle_position))
+                  print(aktuelle_zeit, aktuelle_position)
+        else : #back
+            if (aktuelle_position - 1 == 0):
+                neuer_weg.append((aktuelle_zeit + 1, aktuelle_position-1))
+            else:
+                for i in range (2, aktuelle_position+1):
+                   if  quader_zustand[aktuelle_zeit + 1][aktuelle_position-i] == False:
+                       neuer_weg.append((aktuelle_zeit + 1, aktuelle_position-i))
+                       break
+                
+                print(aktuelle_zeit, aktuelle_position)
         wege.append(neuer_weg)
 
     # Finde den schnellsten Weg
@@ -70,7 +88,7 @@ def berechne_weg(perioden):
                 if position ==   anzahl_abschnitte - 1:    
                     instruktionen.append( "Warte " + str(zeit - start_zeit) + ", laufe zum Grabmal")
                 else:         
-                    instruktionen.append(( "Warte " + str(zeit - start_zeit) + ", laufe in Abschnitt " + str(position + 1)))
+                    instruktionen.append(( "Warte " + str(zeit - start_zeit) + ", laufe in Abschnitt " + str(position)))
                 start_zeit = zeit
         return instruktionen
     else:
@@ -78,7 +96,7 @@ def berechne_weg(perioden):
 
 # Aufruf
 #perioden = [5, 8, 12, 16, 21,73]
-perioden = get_perioden("grabmal4.txt")
+last_quader, perioden = get_perioden("grabmal0.txt")
 
 instruktionen = berechne_weg(perioden)
 
